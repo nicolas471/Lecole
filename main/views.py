88e9b2648ff8eb6d896d062b9be0c9118c2001b5
Nicolas Stefani <nicolas471@openmailbox.org>
2016-03-17 20:33:28 -0300

    limit lista_eventos between dates

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
+import datetime
 from django.shortcuts import render
+
 from models import Evento
 
 def carta(request):
@@ -6,9 +8,19 @@ def carta(request):
 
 
 def evento(request):
-    lista_eventos = Evento.objects.all()
+    lista_semana = []
+    ahora = datetime.datetime.now()
+    futuro = ahora + datetime.timedelta(days=5)
+
+    ahora = datetime.datetime.date(ahora)
+    futuro = datetime.datetime.date(futuro)
+
+    for evento in Evento.objects.all():
+        if evento.fecha_evento > ahora and evento.fecha_evento < futuro:
+            lista_semana.append(evento)
+
     return render(request, 'main/base_evento.html',
-                    {'lista_eventos': lista_eventos})
+                    {'lista_eventos': lista_semana})
 
 
 def detalle_evento(request):
