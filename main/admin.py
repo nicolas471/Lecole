eca2ea3b0e4003f06d03603ed12e8a1e0543b374
nicolas471 <nicolas471@gmail.com>
2016-04-13 22:11:56 -0300

    minor changes

--- main/admin.py
+++ main/admin.py
@@ -4,7 +4,7 @@ from models import Tipo, Artista, Evento, Menu, Horario, GeneralSetting
 class ArtistaAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'tipo')
     search_fields = ('nombre',)
-
+    save_on_top = True
 
 class EventoAdmin(admin.ModelAdmin):
 
@@ -19,15 +19,17 @@ class EventoAdmin(admin.ModelAdmin):
     )
 
     list_display = ('nombre', 'fecha_evento')
+    list_per_page = 31
     list_filter = ('fecha_evento',)
     date_hierarchy = 'fecha_evento'
     filter_horizontal = ('espectaculo',)
     ordering = ('-fecha_evento',)
+    save_on_top = True
 
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'precio', 'promocion')
     ordering = ('hs_dia',)
-
+    save_on_top = True
 admin.site.register(Tipo)
 admin.site.register(Artista, ArtistaAdmin)
 admin.site.register(Evento, EventoAdmin)
